19d53e17387816dea3befbc1594df4b6da8035bb
totiendepzai@gmail.com <107164896+tominhtien2003@users.noreply.github.com>
2024-07-10 15:24:46 +0700

    Updated branch iam_tien

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,12 +17,14 @@ public class CameraController : MonoBehaviour
     {
         offsetDis = Vector3.Distance(target.position, transform.position);
     }
+
     private void LateUpdate()
     {
         HandleRotateWhenSwipe();
         RotateGunBarrel();
         UpdateCameraPosition();
     }
+
     private void HandleRotateWhenSwipe()
     {
         if (Input.GetMouseButton(0) && IsWithinSwipeArea())
@@ -32,14 +34,11 @@ public class CameraController : MonoBehaviour
             if (lastMousePosition != Vector3.zero)
             {
                 Vector3 mouseDelta = (currentMousePosition - lastMousePosition).normalized;
-
                 float rotationAmount = mouseDelta.x * rotateSpeedCameraWhenSwipe * Time.deltaTime;
-
                 target.Rotate(Vector3.up, rotationAmount);
             }
 
             lastMousePosition = currentMousePosition;
-
             isSwiping = true;
         }
         else
@@ -47,65 +46,50 @@ public class CameraController : MonoBehaviour
             ResetSwipeState();
         }
     }
+
     public void ZoomViewfinder()
     {
-        if (type == 0)
-        {
-            Camera.main.fieldOfView = 20;
-        }
-        else
-        {
-            Camera.main.fieldOfView = 60;
-        }
+        Camera.main.fieldOfView = (type == 0) ? 20 : 60;
         type = 1 - type;
     }
+
     private void UpdateCameraPosition()
     {
         if (target == null) return;
 
         Vector3 offset = -target.forward * offsetDis;
-
         offset.y = 6.7f;
 
         transform.position = target.position + offset;
-
         transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z), Vector3.up);
     }
 
     private bool IsWithinSwipeArea()
     {
-        if (isSwiping)
-        {
-            return true;
-        }
+        if (isSwiping) return true;
+
         GameObject obj = EventSystem.current.currentSelectedGameObject;
 
         if (obj != null && obj.CompareTag("AreaSwipeCamera"))
         {
             isSwiping = true;
-
             return true;
         }
 
         return false;
     }
+
     private void RotateGunBarrel()
     {
         if (target == null || gunBarrel == null) return;
-        if (target.forward == gunBarrel.forward)
-        {
-            Debug.Log("Same direction");
-            return;
-        }
 
         Quaternion targetRotation = Quaternion.LookRotation(target.forward);
-
         gunBarrel.rotation = Quaternion.RotateTowards(gunBarrel.rotation, targetRotation, rotateSpeedGunBarrel * Time.deltaTime);
     }
+
     private void ResetSwipeState()
     {
         isSwiping = false;
-
         lastMousePosition = Vector3.zero;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,31 +9,57 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameInput gameInput;
 
     private Rigidbody rb;
+    private Vector2 inputVector;
+    private bool isMoving;
 
-    public bool isMoving;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-    private void Start()
+
+    private void OnEnable()
     {
-        gameInput.playerInputAction.Player.Move.started += Move_started;
-        gameInput.playerInputAction.Player.Move.canceled += Move_canceled;
+        gameInput.playerInputAction.Player.Move.started += OnMoveStarted;
+        gameInput.playerInputAction.Player.Move.canceled += OnMoveCanceled;
     }
 
-    private void Move_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void OnDisable()
+    {
+        gameInput.playerInputAction.Player.Move.started -= OnMoveStarted;
+        gameInput.playerInputAction.Player.Move.canceled -= OnMoveCanceled;
+    }
+
+    private void OnMoveStarted(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        isMoving = true;
+    }
+
+    private void OnMoveCanceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
         isMoving = false;
     }
 
-    private void Move_started(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        isMoving = true;
-    }
     private void FixedUpdate()
     {
         HandleMovement();
     }
+
+    private void HandleMovement()
+    {
+        inputVector = gameInput.GetMovementInputVector();
+        Vector3 moveDirection = transform.forward * inputVector.y;
+
+        rb.velocity = moveDirection * moveSpeed;
+
+        if (inputVector != Vector2.zero)
+        {
+            HandleAnimationWheel();
+
+            Quaternion targetRotation = Quaternion.Euler(0f, inputVector.x * rotateSpeed * Time.deltaTime, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * targetRotation, Time.deltaTime * rotateSpeed);
+        }
+    }
+
     private void HandleAnimationWheel()
     {
         if (isMoving)
@@ -43,30 +69,5 @@ public class PlayerController : MonoBehaviour
                 wheel.Rotate(rotateSpeedOfWheel * Time.fixedDeltaTime, 0f, 0f);
             }
         }
-    }
-    private void HandleMovement()
-    {
-        Vector2 inputVector = gameInput.GetMovementInputVector();
-
-        Vector3 moveDirec = transform.forward * inputVector.y;
-
-        if (inputVector.y != 0f)
-        {
-            HandleAnimationWheel();
-        }
-
-        rb.velocity = moveDirec * moveSpeed;
-
-        if (inputVector != Vector2.zero)
-        {
-            Quaternion targetRotation = Quaternion.Euler(0f, inputVector.x * rotateSpeed * Time.deltaTime, 0f);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * targetRotation, Time.deltaTime * rotateSpeed);
-        }
-    }
-    private void OnDisable()
-    {
-        gameInput.playerInputAction.Player.Move.started -= Move_started;
-        gameInput.playerInputAction.Player.Move.canceled -= Move_canceled;
     }
 }
